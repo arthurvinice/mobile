c9f6f0294ba041fb3fba35bec21ff388bbb4cc51
Arthur <arthurvinice@gmail.com>
2025-04-29 08:46:22 -0300

    add img

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,45 +10,49 @@ export default function HomeScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
+      <ThemedView style={styles.divLogo}>
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/arthurlanches.jpeg')}
+          style={styles.mainLogo}
         />
+      </ThemedView>
+        
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Seja bem-vindo a Arthur Lanches!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Salada Pequena - 100ml</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+          Frutas: 
           <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
+            {'\n'}- Abacaxi
+            {'\n'}- Maçã
+            {'\n'}- Banana
+            {'\n'}- Uva
+            {'\n'}- Morango
+            {'\n'}- VALOR: <ThemedText type='defaultSemiBold'>R$5,00</ThemedText> 
           </ThemedText>{' '}
-          to open developer tools.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText type="subtitle">Vitamina 300ml</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+          Frutas: 
+          <ThemedText type="defaultSemiBold">
+            {'\n'}- Abacaxi
+            {'\n'}- Maçã
+            {'\n'}- Banana
+            {'\n'}- Uva
+            {'\n'}- Morango
+            {'\n'}- VALOR: <ThemedText type='defaultSemiBold'>R$10,00</ThemedText> 
+          </ThemedText>{' '}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <ThemedText type="subtitle">Sanduíche Natural</ThemedText>
+
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -64,7 +68,20 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
+  divLogo:{
+    height: 178,
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 1,
+    backgroundColor: Platform.select({
+      ios: 'rgba(255, 255, 255, 0.5)',
+      android: 'rgba(255, 255, 255, 0.5)',
+      default: 'rgba(255, 255, 255, 0.5)',
+    }),
+  },
+  mainLogo: {
     height: 178,
     width: 290,
     bottom: 0,
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,11 +7,12 @@ import { ThemedView } from '@/components/ThemedView';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: 'Que DEMAIS!!!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+        <ThemedText type="title" style={styles.titulo1}>Essa tela não existe</ThemedText>
+        <ThemedText type="title" style={styles.titulo2}>VOLTE IMEDIATAMENTE</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link" style={styles.nome2}>voltar para o inicio!</ThemedText>
         </Link>
       </ThemedView>
     </>
@@ -24,9 +25,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+    backgroundColor: 'green',
   },
   link: {
     marginTop: 15,
     paddingVertical: 15,
+    color: 'white',
+    textDecorationLine: 'underline',
+  },
+  titulo1: {
+    fontSize: 150,
+    fontWeight: 'bold',
+    color: 'red',
+    paddingBottom: 30,
+  },
+  titulo2: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'blue',
+    marginTop: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: 'yellow',
+    borderRadius: 10,
+  },
+  nome2: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
